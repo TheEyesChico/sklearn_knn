f980506b1f6de2ae7d4c094ff7f8d7675d113e55
Gerivan Santos <gerivantpc3@gmail.com>
2019-03-19 13:06:27 -0300

    Implementation knn with sklearn

--- sklearn_knn.py
+++ sklearn_knn.py
@@ -7,4 +7,20 @@ with open('haberman.data', 'r') as f:
 		atrib = linha.replace('\n', '').split(',')
 		entradas.append([int(atrib[0]), int(atrib[2])])
 		saidas.append(int(atrib[3]))
-print(saidas)
+
+p = 0.6
+limite = int(p*len(entradas))
+knn = KNeighborsClassifier(n_neighbors=15)
+knn.fit(entradas[:limite], saidas[:limite])
+labels = knn.predict(entradas[limite:])
+acertos, indice_label = 0, 0
+
+for i in range(limite,len(entradas)):
+	if labels[indice_label] == saidas[i]:
+		acertos += 1
+	indice_label += 1
+
+print('Total de treinamento: %d' % limite)
+print('Total de testes: %d' % (len(entradas) - limite))
+print('Total de Acertos %d' % acertos)
+print('Percentual de Acertos %.2f' % (100 * acertos / (len(entradas) - limite)))
